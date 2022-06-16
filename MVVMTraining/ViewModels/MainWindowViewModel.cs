4b1076fe6d836ab1eeff619d16beb80f898d8438
Deem <Deem@DEEM>
2022-06-16 15:51:14 +0300

    Добавьте файлы проекта.

---
+++ MVVMTraining/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MVVMTraining.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTraining.ViewModels
+{
+    internal class MainWindowViewModel: ViewModel
+    {
+        private string _nameArticle = "It`s good )";
+        /// <summary>
+        /// Название приложения
+        /// </summary>
+        public string NameArticle
+        {
+            get => _nameArticle;
+            set => Set(ref _nameArticle, value);
+        }
+
+        private int _number;
+        public int Number
+        {
+            get => _number;
+            set => Set(ref _number, value);
+        }
+
+    }
+}
